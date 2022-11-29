92c7b01a8c6a12d9b969b1cb2931ecc632f54664
M <admin@ms-macbook-pro.devices.brown.edu>
2022-11-29 17:30:49 -0500

    add stuff related to data loading.

--- cnn.py
+++ cnn.py
@@ -1,10 +1,16 @@
-from typing import Optional, Dict, List
-import tensorflow as tf
+from __future__ import annotations
+from typing import Optional, Dict, List, Tuple
 import os
+
+import tensorflow as tf
+import cv2
+import numpy as np
 
 DATA_DIR = "/data/images/"
 
 IMAGE_PATH = os.getcwd() + DATA_DIR
+
+T_TEST_TRAIN = Tuple[np.array, np.array, np.array, np.array]
 
 class ImagePot:
     # parsed from abbreviations.csv
@@ -32,7 +38,7 @@ class ImagePot:
         'PMO': 'Promyelocyte',
     }
 
-    def __init__(self, image_paths: List[str], classname: str, encoding: Optional[List[int]] = None):
+    def __init__(self, image_paths: List[str], classname: str, encoding: Optional[List[int]] = None) -> ImagePot:
         self.image_paths = image_paths
         self.classname = classname
         self.encoding = encoding
@@ -40,6 +46,25 @@ class ImagePot:
 
     def set_encoding(self, encoding) -> None:
         self.encoding = encoding
+
+    def get_test_train_split(self, split: float = 0.8) -> T_TEST_TRAIN:
+        n = len(self.image_paths)
+        div = int(n * split)
+
+        labels = [self.encoding] * n
+
+        # split them up
+        train_labels, test_labels = np.array(labels[:div]), np.array(labels[div:])
+        train, test = self.image_paths[:div], self.image_paths[div:]
+        
+        # load the images
+        train = np.array([cv2.imread(x) for x in train])
+        test = np.array([cv2.imread(x) for x in test])
+
+        # convention in our assignments
+        X0, X1 = train, test
+        Y0, Y1 = train_labels, test_labels
+        return X0, Y0, X1, Y1
 
     def __str__(self) -> str:
         return "Image Pot containing Class {0} ({1}), {2} file(s)".format(
@@ -88,6 +113,71 @@ def load_images(
         good_folders[k] = good_folders[k]['pot']
 
     return good_folders
+
+def get_nn_data(
+        data_repo: Dict, 
+        load_classes: List[str] = None,
+        test_train_split: float = 0.8, 
+        shuffle: bool = False
+) -> T_TEST_TRAIN:
+    # default to all classes
+    load_classes = load_classes or list(data_repo.keys())
+
+    # train
+    X0 = np.array([])
+    Y0 = np.array([])
+
+    # test
+    X1 = np.array([])
+    Y1 = np.array([])
+    
+    total_classes = len(load_classes)
+    for i, x in enumerate(data_repo.items()):
+        class_name, pot = x
+        
+        if class_name not in load_classes:
+            # skip it
+            continue
+            
+
+        print("Loading images in {} ({}/{})".format(class_name, i + 1, total_classes))
+
+        x0, y0, x1, y1 = pot.get_test_train_split(test_train_split)
+        # TRAIN
+        X0 = x0 if X0.shape[0] == 0 else np.append(X0, x0, axis=0)
+        Y0 = y0 if Y0.shape[0] == 0 else np.append(Y0, y0, axis=0)
+
+        # TEST
+        X1 = x1 if X1.shape[0] == 0 else np.append(X1, x1, axis=0)
+        Y1 = y1 if Y1.shape[0] == 0 else np.append(Y1, y1, axis=0)
+
+    print("loaded the following: X0: {0}, Y0: {1}, X1: {2}, Y1: {3}". format(
+        X0.shape,
+        Y0.shape,
+        X1.shape,
+        Y1.shape,
+    ))
+
+    X0, Y0 = shuffle_data_and_labels(X0, Y0)
+    X1, Y1 = shuffle_data_and_labels(X1, Y1)
+
+    return X0, Y0, X1, Y1
+
+def shuffle_data_and_labels(data: np.array, labels: np.array) -> Tuple[np.array, np.array]:
+    x = data.shape[0]
+    y = labels.shape[0]
+
+    if x != y:
+        raise ValueError("Mismatch between number of data and number of labels")
+
+    idx = tf.random.shuffle(tf.range(start=0, limit=x, dtype=tf.int32))
+    s_d = tf.gather(data, idx)
+    s_l = tf.gather(labels, idx)
+    return s_d, s_l
+
+def load_data_for_training(load_classes: List[str]) -> T_TEST_TRAIN:
+    images = load_images()
+    return get_nn_data(images, load_classes, 0.8, shuffle=True)
 
 
 def build_cnn(num_classes: int, image_dim: int = 32):
@@ -171,6 +261,7 @@ def train_cnn(X_train, X_test, Y_train, Y_test, model, epochs=10, batch_size=100
     return model
 
 if __name__ == "__main__":
-    data_directory = load_images()
-    from pprint import pprint
-    pprint(data_directory)
+    load_classes = ["ART", "BAS"]
+    X0, Y0, X1, Y1 = load_data_for_training(load_classes)
+
+    print(X0)
